6aefa725a9e8b1931a7e8211f3fad8743b05dcd5
Zane Wan <zwan0238@gmail.com>
2024-02-27 21:53:01 +1100

    contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,11 +1,96 @@
-import React from 'react'
+"use client";
+import { motion } from "framer-motion";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactPage = () => {
-  return (
-    <div>
-      Contact Page
-    </div>
-  )
-}
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const text = "Get in Touch";
 
-export default ContactPage
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setError(false);
+    setSuccess(false);
+
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_SERVICE_ID,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        form.current,
+        process.env.NEXT_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          setSuccess(true);
+          form.current.reset();
+        },
+        () => {
+          setError(true);
+        }
+      );
+  };
+
+  return (
+    <div className="h-full">
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 items-center p-5">
+        {/* TEXT CONTAINER */}
+        <div className="h-1/4 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
+          <div>
+            {text.split("").map((letter, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{
+                  duration: 3,
+                  repeat: Infinity,
+                  delay: index * 0.1,
+                }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+            📩
+          </div>
+        </div>
+        {/* FORM CONTAINER */}
+        <form
+          onSubmit={sendEmail}
+          ref={form}
+          className="h-3/4 lg:h-3/4 lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24"
+        >
+          <span>Hi Zane,</span>
+          <textarea
+            rows={6}
+            className="bg-transparent border-b-2 border-b-black outline-none resize-none lg:h-auto h-32"
+            name="user_message"
+          />
+          <span>Email address to reply to</span>
+          <input
+            name="user_email"
+            type="text"
+            className="bg-transparent border-b-2 border-b-black outline-none"
+          />
+          <button className="bg-purple-200 rounded font-semibold text-gray-600 p-4">
+            Send
+          </button>
+          {success && (
+            <span className="text-green-600 font-semibold">
+              Your message has been sent successfully!
+            </span>
+          )}
+          {error && (
+            <span className="text-red-600 font-semibold">
+              Something went wrong!
+            </span>
+          )}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ContactPage;
